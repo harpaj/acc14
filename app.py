16667dcb258834fba8506a13414339cbfbe734b3
Johannes Huber <huber.johannes@online.de>
2017-10-10 15:20:53 +0200

    Support Parameters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import time
 from tasks import one_angle
 
@@ -8,20 +8,48 @@ app = Flask(__name__)
 @app.route('/run')
 def schedule_run():
     start_time = time.time()
-
-    from_angle = 0
-    to_angle = 30
-    step_size = 10
-    naca = '0012'
-    nodes = '200'
-    refinements = '3'
-    samples = "10"
-    viscosity = "0.0001"
-    speed = "10."
-    total = "1"
+    args = request.args
+    try:
+        from_angle = int(args.get('from_angle'))
+        to_angle = int(args.get('to_angle'))
+        step_size = int(args.get('step_size'))
+        naca = args.get('naca', '0012')
+        assert len(naca) == 4
+        nodes = args.get('nodes', '200')
+        int(nodes)
+        refinements = args.get('refinements', '0')
+        assert 0 <= int(refinements) <= 2
+        samples = args.get('samples', '10')
+        int(samples)
+        viscosity = args.get('viscosity', '0.0001')
+        float(viscosity)
+        speed = args.get('speed', '10.')
+        float(speed)
+        total_time = args.get('total_time', '1')
+        int(total_time)
+    except (TypeError, ValueError, AssertionError) as e:
+        return jsonify({
+            "message": "You delivered invalid parameters.",
+            "required_parameters": {
+                "from_angle": {"type": "int"},
+                "to_angle": {"type": "int"},
+                "step_size": {"type": "int"}
+            },
+            "optional parameters": {
+                "naca": {"type": "char[4]", "default": "0012"},
+                "nodes": {"type": "int", "default": "20"},
+                "refinements": {"type": "[0, 1, 2]", "default": "0"},
+                "samples": {"type": "int", "default": "10"},
+                "viscosity": {"type": "float", "default": "0.0001"},
+                "speed": {"type": "float", "default": "10."},
+                "total_time": {"type": "int", "default": "1"}
+            },
+            "python_error": str(e)
+        })
 
     for angle in range(from_angle, to_angle+1, step_size):
-        task = one_angle.delay(str(angle), *naca, nodes, refinements, samples, viscosity, speed, total)
+        task = one_angle.delay(
+            str(angle), *naca, nodes, refinements, samples, viscosity, speed, total_time)
         break
 
     result = task.get()
