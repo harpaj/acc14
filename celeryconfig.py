0c6ff9ecd32cd82aea5e73d794689c7e426dc865
Johannes Huber <huber.johannes@online.de>
2017-10-17 17:59:20 +0200

    Homogenize Cloud-Init scripts

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,2 +1,2 @@
-broker_url = 'pyamqp://myuser:mypassword@192.168.1.41:5672/myvhost'
-result_backend = 'rpc://myuser:mypassword@192.168.1.41:5672/myvhost'
+broker_url = 'pyamqp://myuser:mypassword@localhost:5672/myvhost'
+result_backend = 'rpc://myuser:mypassword@localhost:5672/myvhost'
