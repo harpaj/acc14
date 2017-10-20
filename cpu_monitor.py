79fb37fd57c2497fe228df90cef4ab8bc0eae4e0
alekodu <alekodu@gmail.com>
2017-10-20 21:19:34 +0200

    rabbit myuser admin permission given

--- cpu_monitor.py
+++ cpu_monitor.py
@@ -1,8 +1,8 @@
 import time, threading
 import subprocess
 
-period = 120
-evaluations = 2
+period = 600
+evaluations = 10
 
 def monitor_cluster_cpu(avg_demand, eval_counter):
     cons = 0
@@ -11,10 +11,10 @@ def monitor_cluster_cpu(avg_demand, eval_counter):
 
     if eval_counter >= evaluations-1:
         if avg_demand/eval_counter > 0.6:
-            subprocess.run(["curl", "-X", "POST", "-i", "https://hpc2n.cloud.snic.se:8000/v1/signal/arn%3Aopenstack%3Aheat%3A%3Aad5091c4f42e4defb98eb9550f875f4f%3Astacks%2Fairfoil_asg_stack_g14%2F3a1326b1-1ccd-438a-8bc8-172c07f73fe2%2Fresources%2Fscaleup_policy?Timestamp=2017-10-20T16%3A23%3A51Z&SignatureMethod=HmacSHA256&AWSAccessKeyId=c8fcde5637bd447a942c487ab54caabe&SignatureVersion=2&Signature=JLlpeTgA%2BowAC%2BlW1UZYtAG%2Bx5fSPHfS%2BhPv54bTnBA%3D"])
+            subprocess.run(['curl', '-X', 'POST', 'https://hpc2n.cloud.snic.se:8000/v1/signal/arn%3Aopenstack%3Aheat%3A%3Aad5091c4f42e4defb98eb9550f875f4f%3Astacks%2Fairfoil_asg_stack_g14%2F8be3ad1e-d91d-4f90-b355-8a570dc8492d%2Fresources%2Fscaleup_policy?Timestamp=2017-10-20T18%3A16%3A58Z&SignatureMethod=HmacSHA256&AWSAccessKeyId=728253c671ee4e87a70fbcc47e10860f&SignatureVersion=2&Signature=7WHSwpnabGkk5I6b2b1cwx3JxLCVNrvFOJfEG0EeRwk%3D'])
             print("Scaling up")
         elif avg_demand/eval_counter < 0.3:
-            subprocess.run(["curl", "-X", "POST", "-i", "https://hpc2n.cloud.snic.se:8000/v1/signal/arn%3Aopenstack%3Aheat%3A%3Aad5091c4f42e4defb98eb9550f875f4f%3Astacks%2Fairfoil_asg_stack_g14%2F3a1326b1-1ccd-438a-8bc8-172c07f73fe2%2Fresources%2Fscaledown_policy?Timestamp=2017-10-20T16%3A23%3A51Z&SignatureMethod=HmacSHA256&AWSAccessKeyId=12444fc58213478eaa4c7e4f7b1a3487&SignatureVersion=2&Signature=bc8oQOuZ8MSH1ZRy93WDzXvcgnfZzxADjadUA9saLG0%3D"])
+            subprocess.run(['curl', '-X', 'POST', 'https://hpc2n.cloud.snic.se:8000/v1/signal/arn%3Aopenstack%3Aheat%3A%3Aad5091c4f42e4defb98eb9550f875f4f%3Astacks%2Fairfoil_asg_stack_g14%2F8be3ad1e-d91d-4f90-b355-8a570dc8492d%2Fresources%2Fscaledown_policy?Timestamp=2017-10-20T18%3A16%3A58Z&SignatureMethod=HmacSHA256&AWSAccessKeyId=ba57b09f02184b8d9bad7846e629c38b&SignatureVersion=2&Signature=Hztm%2FE825s267Obud41qPWV%2BmxV7qVNNJ13TZwPJXaw%3D'])
             print("Scaling down")
         else:
             print("Idle")
@@ -24,12 +24,13 @@ def monitor_cluster_cpu(avg_demand, eval_counter):
 
     rabbit_output = subprocess.check_output(["rabbitmqctl", "list_queues", "-p", "myvhost", "name", "consumers", "messages"])
     rabbit_list = rabbit_output.decode("UTF-8").splitlines()
-    rabbit = (rabbit_list[1].split("\t"))
+    rabbit = (rabbit_list[2].split("\t"))
     cons = int(rabbit[1])
     queue_size = int(rabbit[2]) + cons
     queued_demand = 1.0 - float(cons/queue_size)
     avg_demand += queued_demand
     eval_counter += 1
+
     threading.Timer(period/evaluations, monitor_cluster_cpu, [avg_demand, eval_counter]).start()
 
 monitor_cluster_cpu(0, 0)
